48bad83a56ae448312697da646e153244316a288
shubhkrmishra <shubhamkrmishra98@gmail.com>
2024-07-24 01:19:56 +0530

    Completed Hospital Management System Project

---
+++ src/hospitalManagementSystem/Doctor.java
@@ -0,0 +1,58 @@
+package hospitalManagementSystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Doctor {
+	private Connection connection;
+	
+	public Doctor(Connection connection) {
+		this.connection = connection;
+	}
+	
+
+	public void viewDoctors() {
+		String query = "select * from doctors";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			System.out.println("Doctors: ");
+			System.out.println("+------------+--------------------+----------+------------+");
+			System.out.println("| Doctor ID  | Name               | Specialization        |");
+			System.out.println("+------------+--------------------+----------+------------+");
+			
+			//Now we have to print data     //Currently data are stored in resultSet instance
+			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				String specialization = resultSet.getString("specialization");
+				System.out.printf("| %-10s | %-18s | %-21s |\n", id, name, specialization);
+				System.out.println("+------------+--------------------+----------+------------+");
+			}
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean getDoctorById(int id) {
+		int count = 0;
+		String query = "SELECT * FROM doctors WHERE id = ?";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, id);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				count++;
+			}else {
+				count=0;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
